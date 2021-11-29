54a6e4d007488af8c072955488b26888dcb9f8f9
Firoj <mymindhungry@gmail.com>
2021-11-29 12:28:03 +0545

    Working with Assets in Flutter & the Pubspec file

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://scontent.fsif1-1.fna.fbcdn.net/v/t1.6435-9/35853466_1048334308667710_3151868476088385536_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_ohc=6cPiGGQwGIUAX_xGBES&_nc_ht=scontent.fsif1-1.fna&oh=0978ba81d711a206686c15a8c1185e3e&oe=61CB0A93'),
+            image: AssetImage('images/zayn.jpeg'),
           ),
         ),
       ),
